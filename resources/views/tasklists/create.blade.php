b5a4a4959a32874c949aa5198680540d347ee190
k_nagata <k_nagata@ism.co.jp>
2018-04-16 08:22:44 +0000

    add status

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -6,6 +6,8 @@
 
 
     {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -4,12 +4,14 @@
 
     <h1>id = {{ $tasklist->id }} のメッセージ詳細ページ</h1>
 
-    <p>{{ $tasklist->content }}</p>
-
+    <p>ステータス: {{ $tasklist->status }}</p>
+    <p>メッセージ: {{ $tasklist->content }}</p>
+    
+    
     {!! link_to_route('tasklists.edit', 'このメッセージ編集', ['id' => $tasklist->id]) !!}
 
     {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
     {!! Form::close() !!}
-
+    
 @endsection
