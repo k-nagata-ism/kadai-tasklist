28e2eda36402afb3177728a5eebf2a1b32ce56f8
k_nagata <k_nagata@ism.co.jp>
2018-04-25 07:58:20 +0000

    222

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', 'TasklistsController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('tasklists', 'TasklistsController');
-
+// ユーザ登録
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
